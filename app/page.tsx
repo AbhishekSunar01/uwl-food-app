4989bb869741b2b02bd43ae132577f43393b9332
nityashahi1 <thakurinitu263@gmail.com>
2026-04-03 22:35:35 +0100

    fix: card size

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import ProductCard from "@/components/product-card"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh flex-col gap-12 p-6">
+    <div className="flex min-h-svh flex-col gap-6 p-6">
       <Hero />
-      <h2 className="mb-6 text-2xl font-semibold">Products</h2>
-      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <h2 className="text-2xl font-semibold">Products</h2>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <ProductCard />
         <ProductCard />
         <ProductCard />
